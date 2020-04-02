ea18e07f76d246b1307a6f0603c4229d5c2ed7a8
alexgr007 <62437255+alexgr007@users.noreply.github.com>
2020-04-02 16:49:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//here code
+const Discord = require('discord.js'); 
+const client = new Discord.Client();
+const config = require('./config.json')
+
+client.on('ready', async () => {
+    console.log('I am ready!'); 
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: 'castHelp',
+            type: "WATCHING",
+        }
+     }); 
+});
+
+const token = 'Njk0ODc3MDYyNDU2MDE2OTc4.XoXtJw.xWI7x5M7ueDDU0TD1VTA8vv7qIM';
+
+client.login(token);
